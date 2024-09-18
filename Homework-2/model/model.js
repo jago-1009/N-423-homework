e561ab42e8a97938bc3a095c100bdb4a0a364d6d
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-09-17 21:36:26 -0400

    Added Hourly Functionality
    Added change to days so it shows in a weekly format and added daily menu.

--- Homework-2/model/model.js
+++ Homework-2/model/model.js
@@ -1,7 +1,9 @@
 import { APIKEY } from "./api.js";
 import { APIURL } from "./api.js";
-let trail = "&days=5"
-
+let trail = "&days=7"
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+let isCelsius = false
+let isFahrenheit = true
 export function listenForm() {
     $("#forecastSearch").click(
         function (e) {
@@ -12,14 +14,76 @@ export function listenForm() {
     
 }
 
+
+
 function getForecast(area) {
+    $('#degreeType').click(function() {
+        isFahrenheit = !isFahrenheit;
+        isCelsius = !isCelsius;
+        $('#degreeType').html(isCelsius ? 'C&deg;' : 'F&deg;')
+        })
+    
 $.getJSON(APIURL + area + trail,function(data) {
+    
 $("#results").html(
-    `<h5>Forecast for ${data.location.name}, ${data.location.region}</h5>`
+    `<h5 class="title">Forecast for ${data.location.name}, ${data.location.region}</h5>
+    <h5 class="country">${data.location.country}</h5>
+    <div class="forecast">
+    </div>`
 );
-console.log(data)
+$.each(data.forecast.forecastday, function (idx, day) { 
+    
+    let dayDate = new Date(day.date)
+    let dayOfWeek = dayDate.getDay()
+    // console.log(dayOfWeek)
+    // console.log(day)
+    $(".forecast").append(`<div class="card" id="${idx}">
+    <img src="${day.day.condition.icon}" alt="${day.day.condition.text}" height = "100px" >
+    
+    <h4>${days[dayOfWeek]}</h4>
+    
+    ${day.day.daily_will_it_rain == 1 ? `<p>${day.day.daily_chance_of_rain}% Chance of rain</p>` : "" }
+    ${day.day.daily_will_it_snow == 1 ? `<p>${day.day.daily_chance_of_snow}% Chance of snow</p>` : "" }
+    <p class="high-daily">High of ${isFahrenheit == true ? day.day.maxtemp_f : day.day.maxtemp_c}&deg;</p>
+    <p class="low-daily">Low of ${isFahrenheit == true ? day.day.mintemp_f : day.day.mintemp_c}&deg;</p>
+    </div>`)
+   
+})
+$(".card").click(function(e) {
+    getHourlyForecast(data.forecast.forecastday[e.currentTarget.id], data.location.name)
+})
     
 })
    
+
+}
+export function getHourlyForecast(dailyData, area) {
+    console.log(dailyData)
+    $("#results").html(
+        
+        `<div class="title-bar"><i class="fa-solid fa-arrow-left go-back"></i><h5 class="title">Hourly Forecast for ${area}</h5></div>
+    <div class="forecast">`)
+    $.each(dailyData.hour, function (idx, hour) {
+        let timeDate = new Date(hour.time)
+        let timeHour = timeDate.getHours() + 1
+        $(".forecast").append(
+        `<div class="card-hourly">
+        <img src="${hour.condition.icon}" alt="${hour.condition.text}" height = "100px" >
+        <h4>${timeHour> 12 ? `${timeHour - 12}:00 PM` : `${timeHour}:00 AM`}</h4>
+        ${hour.will_it_rain == 1 ? `<p>${hour.chance_of_rain}% Chance of rain</p>` : "" }
+        ${hour.will_it_snow == 1 ? `<p>${hour.chance_of_snow}% Chance of snow</p>` : "" }
+        <p>Temperature: ${hour.temp_f}&deg;</p>
+
+        
+        `)}
+        
+    )
+
+
+        
+    
+$(".go-back").click(function(e) {
+getForecast(area)
+})
 
 }
