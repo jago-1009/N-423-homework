c5154cd8814c168a9c64be5137dd7bc17d9381b4
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-10-17 15:30:10 -0400

    Update firebaseConfig.js

--- Final-Proj/my-app/src/app/firebaseConfig.js
+++ Final-Proj/my-app/src/app/firebaseConfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { PUBLIC_API_KEY, PUBLIC_AUTH_DOMAIN, PUBLIC_PROJECT_ID, PUBLIC_STORAGE_BUCKET, PUBLIC_MESSAGING_SENDER_ID, PUBLIC_APP_ID } from "$env/static/public";
+import { PUBLIC_API_KEY, PUBLIC_AUTH_DOMAIN, PUBLIC_PROJECT_ID, PUBLIC_STORAGE_BUCKET, PUBLIC_MESSAGING_SENDER_ID, PUBLIC_APP_ID, PUBLIC_MSG_KEY } from "$env/static/public";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: PUBLIC_API_KEY,
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const key = process.env.MSG_KEY
+export const key = PUBLIC_MSG_KEY
