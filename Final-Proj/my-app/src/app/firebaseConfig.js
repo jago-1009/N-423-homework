2be3af026af1f4c637da75a81a77854bfe83422f
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-10-17 15:01:23 -0400

    Update firebaseConfig.js

--- Final-Proj/my-app/src/app/firebaseConfig.js
+++ Final-Proj/my-app/src/app/firebaseConfig.js
@@ -4,7 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log(process.env.API_KEY)
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
